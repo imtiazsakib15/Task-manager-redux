07f8a80d88c42d679c8e09ee9d5098218e8bcfa5
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-04-15 22:20:45 +0600

    Add task modal ui created

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
+
 const Tasks = () => {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <div>
       <div className="flex justify-between">
         <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold">Tasks</h2>
-        <button className="bg-green-700 hover:bg-green-800 rounded-md px-4 py-2 text-white">
+        <button
+          onClick={openModal}
+          className="bg-green-700 hover:bg-green-800 rounded-md px-4 py-2 text-white"
+        >
           Add Task
         </button>
+
+        <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-6">
         <div>
